3714ecad31a107312da6b507599d4b32f6cfd7f8
EpicName56 <vladmont96@yandex.ru>
2021-11-08 19:37:30 +0500

    add first program

--- src/main/java/lesson3/Domashka.java
+++ src/main/java/lesson3/Domashka.java
@@ -4,9 +4,11 @@ public class Domashka {
 
     public static void main(String[] args) {
         System.out.println(getTestFact(5) - getTestFact(4));
+        System.out.println(getTestFact(5));
+        System.out.println(getTestFact(3) + getTestFact(2));
     }
-    public static int getTestFact(int f) {
-        int result = 1;
+    public static long getTestFact(int f) {
+        long result = 1;
         for (int a=1; a <= f; a++) {
             result = result * a;
         }
