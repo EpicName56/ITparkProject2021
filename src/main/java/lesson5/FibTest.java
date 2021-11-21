5e6a894cb042d172fde141f7f809cf4095774799
EpicName56 <vladmont96@yandex.ru>
2021-11-21 11:37:46 +0500

    add first program

--- src/main/java/lesson5/FibTest.java
+++ src/main/java/lesson5/FibTest.java
@@ -1,11 +1,9 @@
 package lesson5;
 
-import java.util.Scanner;
-
 public class FibTest {
 
     public static void main(String[] args) {
-        int[] arr = new int[6]; // ввод верхнего номера числа для расчета
+        int[] arr = new int[6];
         arr[0] = 0;
         arr[1] = 1;
         for (int i = 2; i < arr.length; ++i) {
@@ -15,5 +13,7 @@ public class FibTest {
         for (int i = 0; i < arr.length; ++i) {
             System.out.println(arr[i]);
         }
+        System.out.println("Член последовательности с номером 5 - " +
+                "это 6 число в последовательности Фибоначчи");
     }
 }
